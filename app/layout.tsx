a2237c40c32eddccbe2cc4fddd6f71d36abfee92
mahtamun-hoque-fahim <mahtamunhoquefahim@gmail.com>
2025-09-29 15:04:25 +0600

    restart lesson 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,3 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
-import Sidebar from '@/app/ui/dashboard/sidenav';
- 
 export default function RootLayout({
   children,
 }: {
@@ -9,12 +5,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        <div className="flex min-h-screen">
-          <Sidebar />
-          <main className="flex-1 p-6 bg-gray-50">{children}</main>
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -10,8 +10,6 @@ import {
 import { formatCurrency } from './utils';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
-const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
 
 export async function fetchRevenue() {
   try {
@@ -34,12 +32,12 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-const data = await sql<LatestInvoiceRaw[]>`
-  SELECT invoices.amount, customers.name, customers.image_url, customers.email
-  FROM invoices
-  JOIN customers ON invoices.customer_id = customers.id
-  ORDER BY invoices.date DESC
-  LIMIT 5`;
+    const data = await sql<LatestInvoiceRaw[]>`
+      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+      FROM invoices
+      JOIN customers ON invoices.customer_id = customers.id
+      ORDER BY invoices.date DESC
+      LIMIT 5`;
 
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
